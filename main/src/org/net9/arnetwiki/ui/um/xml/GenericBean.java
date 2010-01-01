c85aafd88afb247ae8139b11a835a2d86cc38ee2
Wang Peng <wangp.thu@gmail.com>
2010-01-01 16:30:40 +0000

    重新上传。

---
+++ main/src/org/net9/arnetwiki/ui/um/xml/GenericBean.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package org.net9.arnetwiki.ui.um.xml;
+
+import java.util.Map.Entry;
+
+import org.dom4j.Attribute;
+import org.dom4j.Element;
+
+import com.ociweb.xml.WAX;
+
+/**
+ * @author LeeThree
+ * 
+ */
+public class GenericBean extends Bean {
+
+	public GenericBean(String name) {
+		beanName = name;
+	}
+	
+	/**
+	 * @param ele
+	 */
+	public GenericBean(Element ele) {
+		beanName = ele.getName();
+		id = Long.decode(ele.attributeValue("id"));
+
+		for (Object iter : ele.attributes()) {
+			Attribute attr = (Attribute) iter;
+			putAttribute(attr);
+		}
+		for (Object iter : ele.elements()) {
+			Element elem = (Element) iter;
+			if (elem.isTextOnly())
+				putElement(elem);
+		}
+	}
+
+	public void set(String key, String value) {
+		properties.put(key, value);
+	}
+	
+	public void toXML(WAX wax) {
+		wax.start(beanName);
+		if (get("primary") != null){
+			wax.attr("primary", get("primary"));
+			properties.remove("primary");
+		}
+		if (get("permission") != null){
+			wax.attr("permission", get("permission"));
+			properties.remove("permission");
+		}
+		for (Entry<String, String> property : properties.entrySet()) {
+			wax.child(property.getKey(), property.getValue());
+		}
+		wax.end();
+	}
+}
---
+++ main/src/org/net9/arnetwiki/ui/wiki/WikiPageTest.java
@@ -0,0 +1,95 @@
+package org.net9.arnetwiki.ui.wiki;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+
+/*
+ * @author zym
+ * 
+ */
+
+public class WikiPageTest {
+	private HttpServletRequest request;
+	
+	public WikiPageTest (HttpServletRequest request) {
+		this.request = request;
+	}
+	public String test()
+	{
+		return "Test Success";
+	}
+	public String getID()
+	{
+		return request.getParameter("wiki-id");
+	}
+	public int getType()
+	{
+		return Integer.parseInt(request.getParameter("style"));
+	}
+	public String showPage()
+	{
+		switch(getType())
+		{
+			case 1:		//term
+				return getTermPage();
+			case 2:		//people
+				return getPeoplePage();
+			default:	//institution
+				return getInstitutionPage();
+		}
+	}
+	private String generateWikiItem(String header, String content){
+		return
+			"<div id = '" + header + "' class='groupItem'>" + 
+				"<div class='itemHeader'>" +
+					header +
+					"<a href='#' class='closeEl'>[-]</a>" +
+				"</div>" +
+				"<div class='itemContent'>" + 
+					content +
+				"</div>" +
+			"</div>";
+	}
+	public String getTermPage()
+	{
+		return "You are viewing term:" + getID() + 
+			"<br>" +
+			generateWikiItem("keyword", "test keyword") +
+			generateWikiItem("definition", "test definition") +
+			generateWikiItem("conferences", "test conferences") +
+			generateWikiItem("papers", "test papers") +
+			generateWikiItem("people", "test people") +
+			generateWikiItem("institutions", "test institutions") +
+			generateWikiItem("dataset", "test dataset");
+	}
+	public String getPeoplePage()
+	{
+		return "You are viewing people:" + getID() + 
+			"<br>" +
+			generateWikiItem("keyword", "test keyword") +
+			generateWikiItem("name", "test name") +
+			generateWikiItem("position", "test position") +
+			generateWikiItem("affliation", "test affliation") +
+			generateWikiItem("address", "test address") +
+			generateWikiItem("email", "test email") +
+			generateWikiItem("home-page", "test home-page") +
+			generateWikiItem("paperlist", "test paperlist");
+	}
+	public String getInstitutionPage()
+	{
+		return "You are viewing institution:" + getID() + 
+			"<br>" +
+			generateWikiItem("keyword", "test keyword") +
+			generateWikiItem("location", "test location") +
+			generateWikiItem("peoplelist", "test peoplelist") +
+			generateWikiItem("paperlist", "test paperlist") +
+			generateWikiItem("homepage", "test homepage");			
+	}
+
+
+	private HttpSession getHttpSession() {
+		return request.getSession();
+	}
+	private static final String SESSION_KEY = "ArnetWiki Session";
+}
